0b7080ba3e625ec635511a67276114582d55c534
asomala <somala.abhi@gmail.com>
2021-01-28 22:48:21 +0000

    Made it say diffrent things depending on how far you are if you are lower than the number

--- main.py
+++ main.py
@@ -6,10 +6,9 @@ print(number)
 guess = int(input("Guess a number between 1 and 100: "))
 
 while guess != number:
-  
   if guess < number:
-    if (number - guess) < 2:
-      print("\nI am very very sad. Do you understand how close you are.You just need to go slightly higher.")  
+    if (number - guess) <  2:
+      print("\nDo you understand how close you are. You are very close. You just need to go slightly higher.")  
     elif (number - guess) < 5:
         print("\nWow, you are very close. A little higher.")
     elif (number - guess) < 10:
@@ -18,13 +17,24 @@ while guess != number:
       print("\nYou are still very off. You have to go a lot higher. ")
     elif (number - guess) < 40:
        
-       print("\nWhat are you even doing?? You need to go higher. ")
+       print("\nAlthough you are very very far keep trying. You need to go much much higher. ")
     else:
-      print("\nPlease, never play a guessing game ever. You are soooooo off. You need to go sooooo much higher")
-  elif guess > number:
-    print("\nYou need to guess lower.")
-  else:
-    print("\nMy dude where are you? ")
+      print("\nI'm sorry but you luck is not very good. You need to go sooooo much higher")
+  
+  if guess > number:
+    if (number - guess) > 2:
+      print("\nDo you understand how close you are. You are very close. You just need to go slightly lower.")
+    elif (number - guess) > 5:
+      print("\nWow, you are very close. A little lower.")
+    elif (number - guess) > 10:
+      print("\nYour guess isn't too terribly off. You need to go lower still")
+    elif (number - guess) > 20:
+       print("\nYou are still very off. You have to go a lot lower. ")
+    elif (number - guess) > 40:
+      print("\nAlthough you are very very far keep trying. You need to go much much lower.")
+    else:
+       print("\nI'm sorry but you luck is not very good. You need to go sooooo much lower.")
+  
   guess = int(input("\nGuess a number between 1 and 100: "))
 print("\nYou guessed the number correctly! You Win!")
   
